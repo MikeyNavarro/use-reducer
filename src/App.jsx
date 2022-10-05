d89b936a66e3e94e1212fd1804d6e269973bc983
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-10-04 19:36:05 -0700

    UseReducer()

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComponentWithUseReducer from "./components/ComponentWithUseReducer";
+
+const App = () => {
+  return (
+    <div>
+      <ComponentWithUseReducer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ComponentWithUseReducer.jsx
@@ -0,0 +1,51 @@
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DEPOSIT":
+      return state + action.payload;
+    case "WITHDRAW":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
+const ComponentWithUseReducer = () => {
+  const deposit = (amount) => {
+    dispatch({
+      type: "DEPOSIT",
+      payload: amount,
+    });
+  };
+
+  const withdraw = (amount) => {
+    dispatch({
+      type: "WITHDRAW",
+      payload: amount,
+    });
+  };
+
+  const [amount, dispatch] = useReducer(reducer, 500);
+  return (
+    <div>
+      <h1>{amount}</h1>
+      <button
+        onClick={() => {
+          deposit(50);
+        }}
+      >
+        Deposit
+      </button>
+      <button
+        onClick={() => {
+          withdraw(50);
+        }}
+      >
+        Withdraw
+      </button>
+    </div>
+  );
+};
+
+export default ComponentWithUseReducer;
